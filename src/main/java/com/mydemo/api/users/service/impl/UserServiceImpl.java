b22ed41ea1316254707defc3bc7765d6239cc3f9
KARRTHIKKKK <karthikeyapepeti05@gmail.com>
2026-03-25 19:38:30 +0530

    implementing loadUserByUsername() method

--- src/main/java/com/mydemo/api/users/service/impl/UserServiceImpl.java
+++ src/main/java/com/mydemo/api/users/service/impl/UserServiceImpl.java
@@ -6,11 +6,13 @@ import com.mydemo.api.users.shared.UserDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Service
@@ -45,7 +47,9 @@ public class UserServiceImpl implements UserService
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
     {
-        userRepository.
-        return null;
+        UserEntity userEntity=userRepository.findByEmail(username);
+        if(userEntity==null) throw new UsernameNotFoundException(username);
+        return new User(userEntity.getEmail(),userEntity.getEncryptedPassword(),
+                true,true,true,true,new ArrayList<>());
     }
 }
